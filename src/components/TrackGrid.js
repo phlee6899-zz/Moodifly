a61c01bc84cf4b5b470c5fbf19d0bc1a3a9ab5d3
Philip Lee <phlee6899@gmail.com>
2021-06-12 11:54:26 +0900

    add audio player

--- src/components/TrackGrid.js
+++ src/components/TrackGrid.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import MyLoader from "./ContentLoader";
 import "./TrackGrid.css";
+import Coverflow from "react-coverflow";
 
 export default function TrackGrid({
   topTrackLoading,
@@ -12,6 +13,12 @@ export default function TrackGrid({
   user,
 }) {
   const [playingInd, setPlayingInd] = useState(-1);
+  const [current, setCurrent] = useState({
+    name: "",
+    img: "",
+    id: -1,
+    artists: [],
+  });
 
   useEffect(() => {
     if (user.user !== undefined) {
@@ -29,6 +36,10 @@ export default function TrackGrid({
     }
   }, [topTracks]);
 
+  const formatArtistName = () => {
+    return current.artists.map((artist) => artist.name).join(", ");
+  };
+
   return (
     <div className="TrackGridContainer">
       {/* {!topTrackLoading && topTracks ? (
@@ -40,13 +51,16 @@ export default function TrackGrid({
         [...Array(20)].map(() => {
           return <MyLoader />;
         })}
+
       {topTracks &&
         topTracks.map((each_item, ind) => {
           return (
             <div className="trackContainer">
-              <audio id={ind} muted>
-                <source src={each_item.preview_url} />
-              </audio>
+              <div>
+                <audio id={ind} muted>
+                  <source src={each_item.preview_url} />
+                </audio>
+              </div>
               <img className="trackImage" src={each_item.album.images[0].url} />
               <div className="overlay" />
               <a>
@@ -63,9 +77,15 @@ export default function TrackGrid({
                       document.getElementById(ind).currentTime = 0;
                     } else {
                       if (0 <= playingInd) {
-                        document.getElementById(playingInd).pause();
                         document.getElementById(playingInd).muted = true;
+                        document.getElementById(playingInd).pause();
                       }
+                      setCurrent({
+                        name: each_item.name,
+                        img: each_item.album.images[0].url,
+                        id: ind,
+                        artists: each_item.artists,
+                      });
                       setPlayingInd(ind);
                       document.getElementById(ind).muted = false;
                       document.getElementById(ind).play();
@@ -111,6 +131,37 @@ export default function TrackGrid({
             </div>
           );
         })}
+
+      <div className="player">
+        <div className="currentImageContainer">
+          {current.name && <img className="currentImage" src={current.img} />}
+        </div>
+        {current.name && (
+          <div className="songInfoContainer">
+            <div className="songName">{current.name}</div>
+            <div className="artistName">{formatArtistName()}</div>
+          </div>
+        )}
+        {current.name && (
+          <div className="playButtonContainer">
+            <a>
+              <img
+                className="playButton"
+                src={
+                  "https://www.pinclipart.com/picdir/big/217-2172073_free-music-icons-play-button-icon-minimalist-clipart.png"
+                }
+                onClick={() => {
+                  if (!document.getElementById(playingInd).paused) {
+                    document.getElementById(playingInd).pause();
+                  } else {
+                    document.getElementById(playingInd).play();
+                  }
+                }}
+              />
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
