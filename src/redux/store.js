f43dccd17111193ec3fd075a932bf4ce0e7b6404
Philip Lee <phlee6899@gmail.com>
2021-07-15 19:14:57 +0900

    encrpytion

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,14 +5,31 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage/session";
 import { routerMiddleware } from "connected-react-router";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 import thunk from "redux-thunk";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
 
-const enhancedReducer = persistReducer(persistConfig, reducer);
+// const enhancedReducer = persistReducer(persistConfig, reducer);
+
+const enhancedReducer = persistReducer(
+  {
+    key: "root",
+    storage,
+    transforms: [
+      encryptTransform({
+        secretKey: "my-super-secret-key",
+        onError: function (error) {
+          // Handle the error.
+        },
+      }),
+    ],
+  },
+  reducer
+);
 
 const store = createStore(
   enhancedReducer,
